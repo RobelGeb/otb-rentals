216f9b01f251c22285d4d196ed864f876f3d8467
bphan23 <bphan23@uw.edu>
2023-08-20 13:48:03 -0700

    create react app and moved over all previous content into react and components form.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div class="centered">
+          <div id="glow-text">
+            Out The Box
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
