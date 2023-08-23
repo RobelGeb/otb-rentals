20a5e283a1ad1fc12f781d19e87ca3032e6f21cb
bphan23 <bphan23@uw.edu>
2023-08-22 19:56:14 -0700

    latest version

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 //import the state hook function
 // import React, { useState } from 'react';
 
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+// import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
-import Footer from './components/Footer';
+// import AboutPage from './components/AboutPage';
+// import Footer from './components/Footer';
 
 function App() {
   return (
