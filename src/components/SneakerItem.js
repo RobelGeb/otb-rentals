19d8c3c72e10c0b190012bdd5c90e9b9b9bff703
rgebr <robelgeb@uw.edu>
2023-08-25 13:01:16 -0700

    sneaker item init

--- src/components/SneakerItem.js
+++ src/components/SneakerItem.js
@@ -1,20 +1,13 @@
-import sneaker from '../img/blue-jordan-1.png';
 import './SneakerItem.css';
 
 function SneakerItem(props) {
-    const colors = props.colors;
     return (
         <div>
             <div>
-                <img id="shoe" src={ sneaker } alt="shoe" />
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-
-                    
-                </ul>
+                <h3>{props.brand} {props.model}</h3>
+                <img id="shoeImage" src={props.sneakerImage} alt="sneaker" />
+                <p>{ props.condition }</p>
+                <p>{ props.price }</p>
             </div>
         </div>
     );
