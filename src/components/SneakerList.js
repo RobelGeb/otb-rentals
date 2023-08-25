23ffe5ceb6e5d07e8849e64cdac127dfda3551a6
rgebr <robelgeb@uw.edu>
2023-08-25 13:27:51 -0700

    latest version sneaker

--- src/components/SneakerList.js
+++ src/components/SneakerList.js
@@ -1,9 +1,16 @@
 import SneakerItem from './SneakerItem.js';
+import sneakerpic from '../img/yeezy700.jpg'
 
 function SneakerList() {
   return(
     <div>
-        <SneakerItem brand="Nike" model="Air Force 1" SneakerItem="" condition="Good" price="$50"/>
+        <SneakerItem 
+            brand="Nike" 
+            model="Air Force 1" 
+            sneakerImage={sneakerpic} 
+            condition="Good" 
+            price="$50"
+        />
     </div>
   );
 }
