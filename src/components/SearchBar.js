512a8bb26f7eabe4478611e33340b0c2e7eb0643
RobelGeb <robelgeb@uw.edu>
2023-08-30 21:55:46 -0700

    search icon fontawesome import

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,12 @@
 import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 function SearchBar() {
   return (
     <div id="search-bar-container">
       <input id="search-bar-input" placeholder="Search sneaker here..."/>
-      <button id="search-btn">Search</button>
+      <button id="search-btn"> <FontAwesomeIcon icon={faMagnifyingGlass} /> </button>
       <select id="filter">
         <option value="all">Select a type</option>
         <option value="nike">Nike</option>
