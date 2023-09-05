1422c83c8fe919b5a9234441b6475634a20a02f1
rgebr <robelgeb@uw.edu>
2023-09-05 13:28:33 -0700

    changed prod page route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Route path="/" element={<HomePage/>} />
       <Route path="/about" element={<AboutPage/>} />
       <Route path="/rentals" element={<RentalsPage/>} />
-      <Route path="yeezy350bred" element={<ProductPage />} />
+      <Route path="/shoe" element={<ProductPage />} />
     </Routes>
   );
 }
