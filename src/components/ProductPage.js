5a9098d75de1b18a6f543c217293111ba727947c
RobelGeb <robelgeb@uw.edu>
2023-09-05 23:01:23 -0700

    json img urls

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -5,10 +5,11 @@ import AddButton from './AddButton';
 import './ProductPage.css';
 import ProductCarousel from './ProductCarousel';
 import SearchBar from './SearchBar';
+import Sneakers from '../data.json';
 
 // import yeezy350pics
 import yeezy350side from '../img/sneakers/yeezy350/yeezy350bred_side.png';
-import yeezy350rear from '../img/sneakers/yeezy350/yeezy350bred_rear.png'
+import yeezy350rear from '../img/sneakers/yeezy350/yeezy350bred_rear.png';
 
 function ProductPage (props) {
 
@@ -17,46 +18,46 @@ function ProductPage (props) {
         {
           id: 0,
           name: "yeezy350",
-          original: yeezy350,
-          thumbnail: yeezy350,
+          original: Sneakers.sneakers[1].sneakerImage,
+          thumbnail: Sneakers.sneakers[1].sneakerImage,
           quantity: 0
         },
         {
           id: 1,
           name: "yeezy700",
-          original: yeezy700,
-          thumbnail: yeezy700,
+          original: Sneakers.sneakers[0].sneakerImage,
+          thumbnail: Sneakers.sneakers[0].sneakerImage,
           quantity: 0
         },
         {
           id: 2,
           name: "uncOw1",
-          original: uncOw1,
-          thumbnail: uncOw1,
+          original: Sneakers.sneakers[12].sneakerImage,
+          thumbnail: Sneakers.sneakers[12].sneakerImage,
           quantity: 0
         },
     ];
 
-    const yeezy350pics = [
-        {
-            id: 0,
-            thumbnail: yeezy350,
-            name: 'yeezy350'
-        },
-        {
-            id: 1,
-            thumbnail: yeezy350side,
-            name: 'yeezy350'
-        },
-        {
-            id: 2,
-            thumbnail: yeezy350rear,
-            name: 'yeezy350'
-        }
-    ];
+    // const yeezy350pics = [
+    //     {
+    //         id: 0,
+    //         thumbnail: yeezy350,
+    //         name: 'yeezy350'
+    //     },
+    //     {
+    //         id: 1,
+    //         thumbnail: yeezy350side,
+    //         name: 'yeezy350'
+    //     },
+    //     {
+    //         id: 2,
+    //         thumbnail: yeezy350rear,
+    //         name: 'yeezy350'
+    //     }
+    // ];
 
     //which image is currently the thumbnail
-    const[sliderData, setSliderData] = useState(yeezy350pics[0]);
+    // const[sliderData, setSliderData] = useState(yeezy350pics[0]);
 
     return (
         <div id="product">
@@ -67,7 +68,7 @@ function ProductPage (props) {
                     <AddButton
                         cart={props.cart}
                         setCart={props.setCart}
-                        shoeName={yeezy350pics[0].name}
+                        
                     />
                 </div>
             </div>
