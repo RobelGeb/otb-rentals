631115587d154e83e2ae085f063676fff5331291
bphan23 <bphan23@uw.edu>
2023-09-21 19:16:54 -0700

    added styling to support page

--- src/components/SupportPage.js
+++ src/components/SupportPage.js
@@ -1,6 +1,7 @@
 import './SupportPage.css';
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import NavBar from './NavBar';
 
 function SupportPage() {
 
@@ -30,49 +31,55 @@ function SupportPage() {
   };
 
   return (
-    <div id="support-container">
+    <div>
 
-      <div id="support-intro">
-        <h2>Out The Box Support Team</h2>
-        <p>For any questions please fill out the form down below, we will get
-          back to you as soon as we can!
-        </p>
-      </div>
+      <NavBar />
 
-      <div id="support-form">
-        <form ref={form} onSubmit={handleSubmit}>
-          <div id="form-inputs-div">
-            <label>Name</label>
-            <input
-              type="text"
-              placeholder="Enter Name"
-              name="user_name"
-              onChange={event => {
-                setName(event.target.value);
-            }}/>
-            <label>Email</label>
-            <input
-              type="text"
-              placeholder="Enter Email"
-              name="user_email"
-              onChange={event => {
-                setEmail(event.target.value)
+      <div id="support-container">
+        <div id="support-intro">
+          <h2>Out The Box Support Team</h2>
+          <p>For any questions please fill out the form down below, we will get
+            back to you as soon as we can!
+          </p>
+        </div>
+
+        <div id="support-form">
+          <form ref={form} onSubmit={handleSubmit}>
+            <div id="form-inputs-div">
+              <label>Name</label>
+              <input
+                id="name_input"
+                type="text"
+                placeholder="Enter Name"
+                name="user_name"
+                onChange={event => {
+                  setName(event.target.value);
               }}/>
-            <label>Message</label>
-            <textarea
-              name="message"
-              type="text"
-              placeholder="Enter Message"
-              onChange={event => {
-                setMessage(event.target.value);
-              }}/>
-          </div>
-          <div id="submit-input-div">
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
-      </div>
+              <label>Email</label>
+              <input
+              id="email_input"
+                type="text"
+                placeholder="Enter Email"
+                name="user_email"
+                onChange={event => {
+                  setEmail(event.target.value)
+                }}/>
+              <label>Message</label>
+              <textarea
+                name="message"
+                type="text"
+                placeholder="Enter Message"
+                onChange={event => {
+                  setMessage(event.target.value);
+                }}/>
+            </div>
+            <div id="submit-input-div">
+              <input id="submit-input-btn" type="submit" value="Submit" />
+            </div>
+          </form>
+        </div>
 
+      </div>
     </div>
   )
 }
